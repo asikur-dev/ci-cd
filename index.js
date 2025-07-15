1fe2d631a6fb9d656d10fb7c92efdc080b3a489b
asikur-dev <ashikur@orangetoolz.com>
2025-07-15 15:28:08 +0600

    Initial commit with CI/CD setup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("Hello from your VPS!"));
+app.get("/", (req, res) => res.send("Hello from your VPSs!"));
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`🟢 Listening on ${PORT}`));
